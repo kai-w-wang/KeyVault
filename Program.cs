f567c47b50435722dbe975c95a3b7bd1279d7c82
Kai Wang <kai_wang_@outlook.com>
2020-02-09 12:59:14 +0800

    Host.CreateDefaultBuilder(args)

--- Program.cs
+++ Program.cs
@@ -12,23 +12,7 @@ namespace TestKeyVault
     class Program
     {
         public static async Task Main(string[] args) {
-            // .NET Generic Host
-            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-2.2
-            var host = new HostBuilder()                
-                .ConfigureAppConfiguration((hostContext, configBuilder) => {
-                    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.2
-                    // A typical sequence of configuration providers is:
-                    // - Files(appsettings.json, appsettings.{ Environment}.json, where { Environment} is the app's current hosting environment)
-                    // - Azure Key Vault
-                    // - User secrets(Secret Manager)(Development environment only)
-                    // - Environment variables
-                    // - Command-line arguments
-                    configBuilder.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                    .AddJsonFile("appsettings.json", optional: true)
-                    .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true)
-                    .AddEnvironmentVariables()                    
-                    .AddCommandLine(args);
-                })
+            var host = Host.CreateDefaultBuilder(args)
                 .ConfigureServices(ConfigureServices)
                 .Build();
             await host.RunAsync();
